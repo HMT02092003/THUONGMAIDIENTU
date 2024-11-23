d7cc16e111fd36dc47afa91b0636e6aeb2f10cb8
hoang.manh.toan <hoang.manh.toan@mqsolutions.com.vn>
2024-11-23 15:35:48 +0700

    update: Giao diện

--- src/app/main-layout.tsx
+++ src/app/main-layout.tsx
@@ -1,9 +1,7 @@
-// src/components/layout/MainLayout.tsx
 "use client";
 
 import React from 'react';
 import { Layout, Menu } from 'antd';
-import { UserOutlined, HomeOutlined, SettingOutlined } from '@ant-design/icons';
 import HeaderPage from './header';
 import FooterPage from './footer';
 
@@ -14,21 +12,23 @@ interface MainLayoutProps {
 }
 
 const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
-    return (
 
-        <Layout style={{ minHeight: '100vh' }}>
+    React.useEffect(() => {
+        document.body.style.margin = '0';
+      }, []);
+      
+    return (
+        <Layout style={{ margin: 0, minHeight: '100vh' }}>
             <Header style={{ background: '#1890ff', color: 'white', display: 'flex', alignItems: 'center' }}>
                 <HeaderPage />
             </Header>
 
             <Layout>
-                <Layout style={{ padding: '24px' }}>
-                    <Content style={{ background: '#fff', padding: 24 }}>
-                        {/* phàn tử con render sau layout */}
-                        {children}
-                        {/* phàn tử con render sau layout */}
-                    </Content>
-                </Layout>
+                <Content >
+                    {/* phàn tử con render sau layout */}
+                    {children}
+                    {/* phàn tử con render sau layout */}
+                </Content>
             </Layout>
 
             <Footer style={{ textAlign: 'center' }}>
